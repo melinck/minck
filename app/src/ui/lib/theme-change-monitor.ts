1531306889c40ee4c1ef5f1cba6f3cb84748ebcb
MELINCK <92787537+melinck@users.noreply.github.com>
2022-05-01 12:58:20 -0400

    Initialize __release-test-fb4c905-490416321

---
+++ app/src/ui/lib/theme-change-monitor.ts
@@ -0,0 +1,41 @@
+import {
+  ApplicableTheme,
+  getCurrentlyAppliedTheme,
+  supportsSystemThemeChanges,
+} from './application-theme'
+import { Disposable, Emitter } from 'event-kit'
+import { ipcRenderer } from 'electron'
+
+class ThemeChangeMonitor {
+  private readonly emitter = new Emitter()
+
+  public constructor() {
+    this.subscribe()
+  }
+
+  private subscribe = () => {
+    if (!supportsSystemThemeChanges()) {
+      return
+    }
+
+    ipcRenderer.on('native-theme-updated', this.onThemeNotificationUpdated)
+
+    ipcRenderer.send('subscribe-native-theme-updated')
+  }
+
+  private onThemeNotificationUpdated = () => {
+    const theme = getCurrentlyAppliedTheme()
+    this.emitThemeChanged(theme)
+  }
+
+  public onThemeChanged(fn: (theme: ApplicableTheme) => void): Disposable {
+    return this.emitter.on('theme-changed', fn)
+  }
+
+  private emitThemeChanged(theme: ApplicableTheme) {
+    this.emitter.emit('theme-changed', theme)
+  }
+}
+
+// this becomes our singleton that we can subscribe to from anywhere
+export const themeChangeMonitor = new ThemeChangeMonitor()
